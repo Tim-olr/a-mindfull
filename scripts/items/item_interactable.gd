10985cf045eb650731456ba2bbc92c23cfc117ac
Tim-olr <timolree1@gmail.com>
2026-02-24 15:57:04 +0100

    texture tweaks and ysort

--- scripts/items/item_interactable.gd
+++ scripts/items/item_interactable.gd
@@ -12,11 +12,3 @@ func _ready() -> void:
 func interacted():
 	GlobalPlayer.inventory.inventory.add_item(item)
 	queue_free()
-
-func _on_mouse_entered() -> void:
-	var tw = create_tween()
-	tw.tween_property(icon.material, "shader_parameter/outline_thickness", 6.0, 0.1)
-
-func _on_mouse_exited() -> void:
-	var tw = create_tween()
-	tw.tween_property(icon.material, "shader_parameter/outline_thickness", 0.0, 0.1)
